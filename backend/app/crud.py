a71298d3cf26c0b2ad4cf2f106a26e5451fba8bf
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-15 11:35:13 +0530

    get method with params on async function with types

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -7,5 +7,10 @@ app = FastAPI()
 def home_route ():
     return {"Message": "Welcome to the home route of server"}
 
+# get method on "/items" with {item_id} path-parameters -> async function
+@app.get("/items/{item_id}")
+async def get_item (item_id: int):
+    return {"item_id": item_id}
+
 import uvicorn
 uvicorn.run(app)
