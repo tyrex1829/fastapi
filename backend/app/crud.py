836f538e91d9229aef097487daad053d82a36cd5
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-15 11:59:55 +0530

    # get async method on /models with {model_name} path-param with defined params in enum class

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class ModelName(str,Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
@@ -11,6 +17,17 @@ def home_route ():
 @app.get("/items/{item_id}")
 async def get_item (item_id: int):
     return {"item_id": item_id}
+
+# get async method on "/models" with {model_name} path-param with defined params in enum class
+@app.get("/models/{model_name}")
+async def get_models(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    
+    if model_name is ModelName.resnet:
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuels"}
 
 import uvicorn
 uvicorn.run(app)
