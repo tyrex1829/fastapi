f77308a1fce0d6ac7f4d16eb340be470d94660a6
Saksham Khare <sakshamkhare19@gmail.com>
2024-11-17 10:54:45 +0530

    complete todo in memory using fastapi

--- backend/app/todo.py
+++ backend/app/todo.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
-
 import uuid
 
 class Item(BaseModel):
@@ -16,7 +15,7 @@ class TodoUpdate(BaseModel):
 app = FastAPI()
 
 todos = [
-    {"id": uuid.uuid4(), "title": "Todo 1", "descripltion": "Desc of todo 1"}
+    {"id": str(uuid.uuid4()), "title": "Todo 1", "description": "Desc of todo 1"}
 ]
 
 @app.get("/todos")
@@ -27,30 +26,35 @@ def all_todos():
 async def new_todo(item: Item):
 
     newTodo = {
-        "id": uuid.uuid4(),
+        "id": str(uuid.uuid4()),
         "title": item.title,
         "description": item.description
     }
-
+    print(newTodo)
     todos.append(newTodo)
 
-    return {"message": "Successfully added new item"}
+    return {"message": "Successfully added new item", "todo": newTodo}
 
 @app.patch("/update-todo/{id}")
 async def update_todo(id: str, todo_update: TodoUpdate):
-    todo = next((t for t in todos if t["id"] == id), None)
+    for todo in todos:
+        if todo["id"] == id:
+            if todo_update.updatedTitle is not None:
+                todo["title"] = todo_update.updatedTitle
+            if todo_update.updatedDescription is not None:
+                todo["description"] = todo_update.updatedDescription
+            return {"message": "Todo updated successfully!", "todo": todo}
+    
+    raise HTTPException(status_code=404, detail="Todo not found")
 
-    if not todo: 
-        raise HTTPException(status_code=404, detail="Todo not found")\
-    
-    if todo_update.updatedTitle is not None:
-        todo["title"] = todo_update.updatedTitle
-    
-    if todo_update.updatedDescription is not None:
-        todo["description"] = todo_update.updatedDescription
-    
-    return todo
-
+@app.delete("/delete-todo")
+async def delete_todo(id: str):
+    for idx, todo in enumerate(todos):
+        if todo["id"] == id:
+            deleted_todo = todos.pop(idx)
+            return {"message": "Todo Deleted Successfully", "todo": deleted_todo}
+        
+    raise HTTPException(status_code=404, detail="todo not found")
 
 import uvicorn
 uvicorn.run(app)
